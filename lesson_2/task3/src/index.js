024238e68a7e655c53afc72fd762db2f83b60bc8
Oleh Hanyak <ohanyak@gmail.com>
2020-04-30 14:15:32 +0300

    add

--- lesson_2/task3/src/index.js
+++ lesson_2/task3/src/index.js
@@ -4,22 +4,30 @@ import './styles.css';
 
 const rootElement = document.querySelector('#root');
 
-const seconds = new Date().getSeconds();
-const backgroundColor = seconds % 2 === 0
-    ? '#fff'
-    : '#000';
+const renderSeconds = time => {
+    const seconds = new Date().getSeconds();
+    const backgroundColor = seconds % 2 === 0
+        ? '#fff'
+        : '#000';
 
-const textColor = seconds % 2 !== 0
-    ? '#fff'
-    : '#000';
+    const textColor = seconds % 2 !== 0
+        ? '#fff'
+        : '#000';
 
-const styles = {
-    color: textColor,
-    backgroundColor
+    const styles = {
+        color: textColor,
+        backgroundColor
+    };
+
+    const element = (
+        <div className="seconds" style={styles}>{'Now is ' + seconds}</div>
+    );
+
+
+    ReactDOM.render(element, rootElement);
+
 };
 
-const element = (
-    <div className="seconds" style={styles}>{ 'Now is ' + seconds }</div>
-);
+renderSeconds(new Date());
 
-ReactDOM.render(element, rootElement);
+setInterval(() => renderSeconds(new Date()), 1000);
