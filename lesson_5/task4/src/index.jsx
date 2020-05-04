1d69062410ac16d0d5777589425c2dbdee89e7a4
Oleh Hanyak <ohanyak@gmail.com>
2020-05-04 16:30:32 +0300

    add

---
+++ lesson_5/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Colors from './Colors';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Colors />, rootElement);
