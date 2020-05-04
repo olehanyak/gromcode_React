9b48cf98f93e5ca6cbc2b72a8c46c13d7966de6a
Oleh Hanyak <ohanyak@gmail.com>
2020-05-04 20:21:18 +0300

    add

--- lesson_5/hw1/src/Toggler.jsx
+++ lesson_5/hw1/src/Toggler.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 
 class Toggler extends Component {
-  
-    changeTextButton(e) {
-      console.log(e)
-      console.log(e.target.textContent = 'Off')
-      e.target.textContent = 'On'
-    };
+    constructor(props) {
+        console.log(props)
+        // console.log(Component)
+        super(props);
+        this.state = {isToggleOn: true};
+    }
+
+    changeTextButton = () => {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+          }));
+        }
 
     render() {
         return (
             <button onClick={this.changeTextButton} className="toggler">
-                Off
+                 {this.state.isToggleOn ? 'ON' : 'OFF'}
             </button>
         );
     }
