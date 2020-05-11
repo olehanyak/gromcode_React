0523d0921f01265ffed4dff6f382f6ca4d40e06c
Oleh Hanyak <ohanyak@gmail.com>
2020-05-11 07:25:55 +0300

    add

---
+++ lesson_8/task3/src/Demensions.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Demensions extends Component {
+    state = {
+        width: null,
+        height: null,
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.onResize);
+
+        const { innerWidth, innerHeight } = window;
+        this.setDemensions(innerWidth, innerHeight);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.onResize);
+    }
+
+    onResize = (e) => {
+        const { innerWidth, innerHeight } = e.target;
+        this.setDemensions(innerWidth, innerHeight);
+    };
+
+    setDemensions = (width, height) => {
+        this.setState({
+            width,
+            height,
+        });
+        document.title = `${innerWidth} x ${innerHeight}`;
+    };
+
+    render() {
+        return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
+    }
+}
+
+export default Demensions;
