1688c5a57dbb7819cad355aa5c7b47cb99f72463
Oleh Hanyak <ohanyak@gmail.com>
2020-05-13 11:29:28 +0300

    add

--- lesson_9/hw1/src/index.jsx
+++ lesson_9/hw1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import UsersList from "./UserList";
+import UsersList from "./UsersList.jsx";
 
 const rootElement = document.querySelector("#root");
 
