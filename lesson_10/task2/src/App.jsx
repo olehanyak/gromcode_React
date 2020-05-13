131fc4a153b1c4b085a11247e803afb7e70f633f
Oleh Hanyak <ohanyak@gmail.com>
2020-05-13 20:01:41 +0300

    add

---
+++ lesson_10/task2/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import ShoppingCart from "./ShoppingCart.jsx";
+import Profile from "./Profile.jsx";
+
+class Page extends Component {
+    state = {
+        userData: {
+            firstName: "Robin",
+            lastName: "Hood",
+        },
+    };
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            userData: {
+                ...this.state.userData,
+                [name]: value,
+            },
+        });
+    };
+
+    render() {
+        const { userData } = this.state;
+
+        return (
+            <div className="page">
+                <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
+                <main className="content">
+                    <ShoppingCart userData={userData} />
+                    <Profile userData={userData} handleChange={this.handleChange} />
+                </main>
+            </div>
+        );
+    }
+}
+
+export default Page;
