07782accbbec057d32baabb671c08bc97a714ffc
Oleh Hanyak <ohanyak@gmail.com>
2020-05-14 14:57:51 +0300

    add

---
+++ lesson_10/hw2/src/ProductRow.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductRow = ({ product }) => {
+    // const { product } = this.props.product;
+    const name = product.stocked ? product.name : <span style={{ color: "red" }}>{product.name}</span>;
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+};
+
+export default ProductRow;
---
+++ lesson_10/hw2/src/ProductTable.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ProductCategoryRow from "./ProductCategoryRow.jsx";
+import ProductRow from "./ProductRow.jsx";
+
+const ProductTable = ({ filterText, inStockOnly, products }) => {
+    // const { filterText } = this.props.filterText;
+    // const { inStockOnly } = this.props.inStockOnly;
+    console.log(products);
+    console.log(inStockOnly);
+
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if (product.name.indexOf(filterText) === -1) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
+        if (product.category !== lastCategory) {
+            rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+        }
+        rows.push(<ProductRow product={product} key={product.name} />);
+        lastCategory = product.category;
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+};
+
+export default ProductTable;
