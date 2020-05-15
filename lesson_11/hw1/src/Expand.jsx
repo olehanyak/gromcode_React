b4d4a82e1761957fe681246e3d8c3561453e6200
Oleh Hanyak <ohanyak@gmail.com>
2020-05-15 20:27:24 +0300

    add

--- lesson_11/hw1/src/Expand.jsx
+++ lesson_11/hw1/src/Expand.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp, faChevronDown } from "@fortawesome/fontawesome-free-solid";
-// import PropTypes from "prop-types";
 
 class Expand extends Component {
     state = {
