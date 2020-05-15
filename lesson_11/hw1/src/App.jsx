203159592927f1b0959f48080196f842973f3491
Oleh Hanyak <ohanyak@gmail.com>
2020-05-15 20:21:09 +0300

    add

--- lesson_11/hw1/src/App.jsx
+++ lesson_11/hw1/src/App.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import Expand from "./Expand.jsx";
 
-const App = () => {
-    return (
-        <div className="app">
-            <Expand title="Title">
-                <p>
-                    Hooks are a new addition in React 16.8. They let you use state and other React features without
-                    writing a class.
-                </p>
-            </Expand>
-        </div>
-    );
-};
-
+class App extends Component {
+    render() {
+        const text = (
+            <p>
+                Hooks are a new addition in React 16.8. They let you use state and other React features without writing
+                a class.
+            </p>
+        );
+        return (
+            <div className="app">
+                <Expand title="Some title">{text}</Expand>
+            </div>
+        );
+    }
+}
 export default App;
