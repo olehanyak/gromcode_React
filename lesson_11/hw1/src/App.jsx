9eaaaa3452f5229d356dab82cace7fe86f887739
Oleh Hanyak <ohanyak@gmail.com>
2020-05-15 16:28:14 +0300

    add

--- lesson_11/hw1/src/App.jsx
+++ lesson_11/hw1/src/App.jsx
@@ -4,7 +4,7 @@ import Expand from "./Expand.jsx";
 const App = () => {
     return (
         <div className="app">
-            <Expand title={"Text"}>
+            <Expand title={"Title"}>
                 <p>
                     Hooks are a new addition in React 16.8. They let you use state and other React features without
                     writing a class.
--- lesson_11/hw1/src/Expand.jsx
+++ lesson_11/hw1/src/Expand.jsx
@@ -2,75 +2,43 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp, faChevronDown } from "@fortawesome/fontawesome-free-solid";
 
-// class Expand extends Component {
-//     state = {
-//         content: false,
-//     };
-
-//     onHide = () => {
-//         this.setState({
-//             content: false,
-//         });
-//     };
-
-//     onShow = () => {
-//         this.setState({
-//             content: true,
-//         });
-//     };
-
-//     render() {
-//         const button = this.state.content ? (
-//             <i className="fas fa-chevron-up"></i>
-//         ) : (
-//             <i className="fas fa-chevron-down"></i>
-//         );
-//         const place = this.state.content && <div className="expand__content">{this.props.children}</div>;
-
-//         return (
-//             <div className="expand border">
-//                 <div className="expand__header">
-//                     <span className="expand__title">{this.props.title}</span>
-//                     <button className="expand__toggle-btn">
-//                         {button}
-//                         <FontAwesomeIcon icon={faChevronDown} onClick={this.onShow} />
-//                         <FontAwesomeIcon icon={faChevronUp} onClick={this.onHide} />
-//                     </button>
-//                 </div>
-//                 {place}
-//             </div>
-//             // </div>
-//         );
-//     }
-// }
-
-// export default Expand;
-
 class Expand extends Component {
     state = {
         content: false,
     };
 
-    showText = () =>
+    onHide = () => {
+        this.setState({
+            content: false,
+        });
+    };
+
+    onShow = () => {
         this.setState({
             content: !this.state.content,
         });
+    };
 
     render() {
-        const button = this.state.content ? <i className="fas fa-arrow-up"></i> : <i className="fas fa-arrow-down"></i>;
-        const content = this.state.content && <div className="expand__content">{this.props.children}</div>;
+        const arrowUp = <FontAwesomeIcon icon={faChevronUp} />;
+        const arrowDown = <FontAwesomeIcon icon={faChevronDown} />;
+
+        const button = this.state.content ? (
+            <i className="fas fa-chevron-up">{arrowUp}</i>
+        ) : (
+            <i className="fas fa-chevron-down">{arrowDown}</i>
+        );
+        const place = this.state.content && <div className="expand__content">{this.props.children}</div>;
 
         return (
             <div className="expand border">
                 <div className="expand__header">
                     <span className="expand__title">{this.props.title}</span>
-                    <button className="expand__toggle-btn" onClick={this.showText}>
+                    <button className="expand__toggle-btn" onClick={this.onShow}>
                         {button}
-                        <FontAwesomeIcon icon={faChevronDown} onClick={this.onShow} />
-                        <FontAwesomeIcon icon={faChevronUp} onClick={this.onShow} />
                     </button>
                 </div>
-                {content}
+                {place}
             </div>
         );
     }
