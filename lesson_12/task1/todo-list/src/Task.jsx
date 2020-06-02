47ded2ef26e129f71c57b74d1c63602fb5091f34
Oleh Hanyak <ohanyak@gmail.com>
2020-06-02 08:19:45 +0300

    add

---
+++ lesson_12/task1/todo-list/src/Task.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+const Task = ({ id, done, text, onChange, onDelete }) => {
+    const listItemClass = classNames("list-item", {
+        "list-item_done": done,
+    });
+    return (
+        <li className={listItemClass}>
+            <input
+                className="list-item__checkbox"
+                type="checkbox"
+                defaultChecked={done}
+                onChange={() => onChange(id)}
+            />
+            <span className="list-item__text">{text}</span>
+            <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
+        </li>
+    );
+};
+
+Task.propTypes = {
+    id: PropTypes.string,
+    done: PropTypes.bool,
+    text: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
+};
+
+Task.defaultTypes = {
+    done: false,
+    text: "",
+};
+
+export default Task;
