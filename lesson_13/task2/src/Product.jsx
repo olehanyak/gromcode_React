b32f544ae70fb42cfe0069e716ba58f64d168de5
Oleh Hanyak <ohanyak@gmail.com>
2020-06-04 07:25:25 +0300

    add

--- lesson_13/task2/src/Product.jsx
+++ lesson_13/task2/src/Product.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
 
-const Product = ({ match }) => {
-    return <div className="product">{`Product is a ${match.params.productId}`}</div>;
+const Product = () => {
+    const { productId } = useParams();
+    return <div className="product">{`Product is a ${productId}`}</div>;
 };
 
 export default Product;
