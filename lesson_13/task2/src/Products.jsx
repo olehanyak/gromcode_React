1cf4d5d93d6d2d28c6406e39691109ac9abbf795
Oleh Hanyak <ohanyak@gmail.com>
2020-06-04 07:27:49 +0300

    add

--- lesson_13/task2/src/Products.jsx
+++ lesson_13/task2/src/Products.jsx
@@ -8,10 +8,10 @@ const Products = ({ match }) => {
             <h1>Products</h1>
             <ul className="navigation">
                 <li className="navigation__item">
-                    <Link to={`${match.url}/book`}>Book</Link>
+                    <Link to={`${match.url}/ball`}>Ball</Link>
                 </li>
                 <li className="navigation__item">
-                    <Link to={`${match.url}/ball`}>Ball</Link>
+                    <Link to={`${match.url}/book`}>Book</Link>
                 </li>
             </ul>
             <Switch>
